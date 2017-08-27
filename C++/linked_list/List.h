e2cd48b2a582377ff870394262b195198f87ab66
Oscar Albornoz <oalbornoz08@gmail.com>
2017-08-27 02:30:43 -0400

    Add Linked List Class

--- C++/linked_list/List.h
+++ C++/linked_list/List.h
@@ -5,84 +5,134 @@
 
 #include "Node.h"
 
-#include <thread>
 #include <iostream>
-
-using namespace std;
 
 template<typename T>
 class List {
+ public:
+  List() : head(nullptr), tail(nullptr), size_(0) {}
+  ~List() {}
+  inline bool is_empty() const {return size_ == 0;}
+  inline size_t size() const {return size_;}
+  inline T back() const { return tail->value(); }
+  inline T front() const { return head->value(); }
+
+  void erase(const size_t &pos);
+  void insert(const size_t &pos, const T &val);
+  void pop_back();
+  void pop_front();
+  void push_back(const T &val);
+  void push_front(const T &val);
+
  private:
   Node<T> *head;
   Node<T> *tail;
   size_t size_;
+};
 
- public:
-  List() : head(nullptr), tail(nullptr), size_(0) {}
-  ~List() {std::cout<<"Destructor de List";
+template<typename T>
+void List<T>::erase(const size_t &pos) {
+  if (pos < size() && !is_empty()) {
+    if (pos == 0) {
+      pop_front();
+    } else if (pos == size()- 1) {
+      pop_back();
+      } else {
+        size_t idx = 1;
+        Node<T> *prev = head;
+
+        while (idx < pos) {
+          prev = prev->next();
+          idx++;
+        }
+
+        Node<T> *current = prev->next();
+        prev->set_next(current->next());
+
+        delete current;
+        size_--;
+      }
+  } else {
+     std::cerr << "Error: List index out of range" << std::endl;
   }
-  inline bool is_empty() {return head == nullptr;}
-  inline size_t size() {return size_;}
+}
 
-  inline T back() {
-    if (tail != nullptr)
-      return tail->value();
-    return 0;
+template<typename T>
+void List<T>::insert(const size_t &pos, const T &val) {
+    if (pos < size()) {
+      if (pos == 0) {
+        push_front(val);
+      } else {
+        Node<T> *prev = head;
+
+        for (int i = 0; i < pos && prev->next(); ++i)
+          prev = prev->next();
+
+        Node<T> *new_node = new Node<T>(val);
+
+        new_node->set_next(prev->next());
+        prev->set_next(new_node);
+        size_++;
+      }
+    } else {
+       std::cerr << "Error: List index out of range" << std::endl;
+    }
   }
 
-  inline T front() {
-    if (head != nullptr)
-      return head->value();
-    return 0;
-  }
+template<typename T>
+void List<T>::pop_back() {
+  if (!is_empty()) {
+    Node<T> *prev = head;
 
-  bool push_back(T val) {
+    while (prev->next() && prev->next() != tail)
+      prev = prev->next();
+
+    delete tail;
+    tail = prev;
+    size_--;
+  }
+}
+
+template<typename T>
+void List<T>::pop_front() {
+  if (!is_empty()) {
+    Node<T> *first_node = head;
+
+    head = head->next();
+    delete first_node;
+    size_--;
+  } else {
+    std::cerr << "Error: List is empty" << std::endl;
+  }
+}
+
+template<typename T>
+void List<T>::push_back(const T &val) {
     Node<T> *new_node = new Node<T>(val);
+
     if (is_empty()) {
       head = tail = new_node;  // Both point to single node
     } else if (head == tail) {
-      head->set_next(new_node); // First node point to second node
-      tail = head->next();  // Tail point to second node
-    } else {
-      tail->set_next(new_node);  // Last node point to new last node
-      tail = tail->next();  // Tail point to new last node
-    }
+        head->set_next(new_node);  // First node point to second node
+        tail = head->next();  // Tail point to second node
+      } else {
+        tail->set_next(new_node);  // Last node point to new last node
+        tail = tail->next();  // Tail point to new last node
+        }
     size_++;
-    return true;
   }
 
-  void pop_back() {
-    if (size_) {
-      Node<T> *current = head;
-      auto tail_ = tail;
+template<typename T>
+void List<T>::push_front(const T &val) {
+  Node<T> *new_node = new Node<T>(val);
 
-      while (current->next() != tail_ and current->next() != nullptr)
-          current = current->next();
+  if (is_empty())
+    tail = new_node;
 
-      delete tail;
-
-      tail = current;
-      size_--;
-    }
-  }
-
-  bool insert(size_t pos, T val) {
-    if (pos < size()) {
-      size_t idx = 0;
-
-      Node<T> *current = head;
-      Node<T> *new_node = new Node<T>(val);
-
-      while(idx < pos and current->next()) {
-        current = current->next();
-        idx++;
-      }
-      new_node->set_next(current->next());
-      current->set_next(new_node);
-      }
-    return false;
-  }
-};
+  new_node->set_next(head);
+  head = new_node;
+  size_++;
+}
 
 #endif  // _HOME_OSCAR_PRACTICES_CPP_LINKED_LIST_LIST_H_
 
--- C++/linked_list/main.cpp
+++ C++/linked_list/main.cpp
@@ -3,26 +3,65 @@
 #include <iostream>
 #include "List.h"
 
-using namespace std;
+using std::cout;
+using std::endl;
 
 int main() {
   List<int> my_list;
 
-  for (int i = 0; i < 1000; ++i)
-  {
-    my_list.push_back(i);
-  }
+  my_list.push_front(2);
+  my_list.push_front(1);
+
+  my_list.push_back(4);
+  my_list.push_back(5);
+  my_list.push_back(6);
+
+  my_list.insert(1, 3);
 
   cout << "Tamaño: " << my_list.size() << endl;
 
+  while (my_list.size()) {
+    cout << my_list.front() << endl;
+    my_list.pop_front();
+  }
+  cout << "Tamaño: " << my_list.size() << endl;
 
-  while(my_list.size()) {
+
+  my_list.push_front(1);
+  my_list.push_back(2);
+  my_list.push_back(4);
+  my_list.push_back(5);
+  my_list.push_front(0);
+  my_list.insert(2, 3);
+
+  my_list.erase(0);
+
+  while (my_list.size()) {
+    cout << my_list.back() << endl;
     my_list.pop_back();
   }
 
   cout << "Tamaño: " << my_list.size() << endl;
 
-  cout << "Dale a tu cuerpo energia macarena"<<endl;
+  my_list.push_front(2);
+  my_list.push_front(1);
+
+  my_list.push_back(4);
+  my_list.push_back(5);
+  my_list.push_back(6);
+
+  my_list.insert(1, 3);
+
+  cout << "Tamaño: " << my_list.size() << endl;
+
+  my_list.erase(1);
+  my_list.erase(1);
+  my_list.erase(1);
+  my_list.erase(1);
+  my_list.erase(1);
+  my_list.erase(0);
+
+  cout << "Tamaño: " << my_list.size() << endl;
 
   return 0;
 }
